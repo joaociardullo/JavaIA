72638a557a30b7e7f1d7f80a4c764af694196947
João Ciardullo <jciardullo.deal@veloe.com.br>
2024-05-06 22:14:37 -0300

    Revert "first commit"
    This reverts commit 674d92c7

--- src/main/java/com/devjoao/springai/model/QuestionRequest.java
+++
@@ -1,8 +0,0 @@
-package com.devjoao.springai.model;
-
-import lombok.Data;
-
-@Data
-public class QuestionRequest {
-    String question;
-}
--- src/main/java/com/devjoao/springai/model/Response.java
+++
@@ -1,9 +0,0 @@
-package com.devjoao.springai.model;
-
-import lombok.Data;
-
-@Data
-public class Response {
-
-    String response;
-}
